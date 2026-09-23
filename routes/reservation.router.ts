b45d7f7b74bf1987cfb5b80c4b5f1d754ca42374
Martin Mabrut <martinmabrut43@gmail.com>
2026-09-23 11:45:05 +0200

    middleware de vérification des datas envoyées par le front

--- routes/reservation.router.ts
+++ routes/reservation.router.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import { reservationController} from "../controllers/reservation.controller.ts";
+import { checkReservationData } from "../middlewares/checkReservationData.ts";
 
 const reservationRouter = express.Router();
 
 // Routes Reservation
-reservationRouter.post("/reservations", reservationController.createReservation);
+reservationRouter.post("/reservations", checkReservationData, reservationController.createReservation);
 reservationRouter.get("/reservations", reservationController.findAllReservations);
 reservationRouter.get("/rerservations/:id", reservationController.findReservationById);
 reservationRouter.put("/reservations/:id", reservationController.updateReservation);
