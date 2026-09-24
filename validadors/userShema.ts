c6e1752322dc097de52ff87e3b96c691f6f854cb
Martin Mabrut <martinmabrut43@gmail.com>
2026-09-24 16:41:31 +0200

    ajout des shemas de validation pour les put de user et room + création du shéma de validation pour role

--- validadors/userShema.ts
+++ validadors/userShema.ts
@@ -7,3 +7,11 @@ export const userSchema = Joi.object({
     password: Joi.string().min(6).required(),
     roleId: Joi.number().required()
 });
+
+export const userUpdateSchema = Joi.object({
+    firstname: Joi.string(),
+    lastname: Joi.string(),
+    email: Joi.string().email(),
+    password: Joi.string().min(6),
+    roleId: Joi.number()
+});
